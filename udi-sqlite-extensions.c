7ea506d117c38a446113683ddd6e6a6bd9f0f00a
Avinash Kurup <681657+avinashkurup@users.noreply.github.com>
2023-10-18 12:53:17 +0530

    fix: add static extension loading

--- udi-sqlite-extensions.c
+++ udi-sqlite-extensions.c
@@ -4,7 +4,8 @@ SQLITE_EXTENSION_INIT1
 #include "sqlite-ulid/dist/release/sqlite-ulid.h"
 
 int udi_sqlite_init_extensions(sqlite3 *db, char **pzErrMsg,
-                               const sqlite3_api_routines *pApi) {
+                               const sqlite3_api_routines *pApi)
+{
   (void)pzErrMsg;
   SQLITE_EXTENSION_INIT2(pApi);
 
@@ -14,9 +15,22 @@ int udi_sqlite_init_extensions(sqlite3 *db, char **pzErrMsg,
   //   return rc;
   // }
   // don't return result until sqlite3_ulid_init is made FFI-safe
-  sqlite3_ulid_init(db, pzErrMsg, pApi);
+  // sqlite3_ulid_init(db, pzErrMsg, pApi);
 
   // If you have more extensions, initialize them here in a similar manner
 
+  // Used from sqlite-ulid/examples/c/demo.c
+  int rc = SQLITE_OK;
+  // sqlite3 *db;
+  sqlite3_stmt *stmt;
+  char *error_message;
+
+  rc = sqlite3_auto_extension((void (*)())sqlite3_ulid_init);
+  if (rc != SQLITE_OK)
+  {
+    fprintf(stderr, "❌ demo.c could not load sqlite3_ulid_init: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return 1;
+  }
   return SQLITE_OK;
 }
